ced6d9297ca39bf338e7ac35ff3debef06bea486
laybelmeyden <trycollens@gmail.com>
2019-10-15 01:17:05 +0300

    also

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.modal').modal();
     $('select').material_select();
+    $('btn_1_1').bind('change', function() {
+        alert('Загружаемый файл слишком велик, загрузите его на googledisk или dropbox и прекрепите ссылку в соответсвующие поле: ' + this.files[0].size / 1024 / 1024 + "MB");
+    });
 });
 document.getElementById("added_news").onclick = () => {
     document.getElementById("ucaht_1").classList.toggle('visible');
@@ -18,6 +21,3 @@ document.getElementById("btn_3").onclick = () => {
 document.getElementById("btn_ok").onclick = () => {
     document.getElementById("modalflat").classList.toggle('visible');
 }
-$('btn_1_1').bind('change', function() {
-    alert('Загружаемый файл слишком велик, загрузите его на googledisk или dropbox и прекрепите ссылку в соответсвующие поле: ' + this.files[0].size / 1024 / 1024 + "MB");
-});
