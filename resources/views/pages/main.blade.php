b9853c7355cd0c5a889663922674a526b0c4b714
laybelmeyden <trycollens@gmail.com>
2021-02-18 23:15:07 +0300

    added/logo

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -175,7 +175,7 @@
         <img src="assets/img/pgrants_logo_gp_gark.png" alt="">
       </div>
       <div>
-        <img src="assets/img/pdas.png" alt="">
+        <img src="assets/img/pdas.svg" alt="">
       </div>
     </div>
   </div>
