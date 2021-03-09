c42641d49a6953b1deada7ac89611d71c17f913c
laybelmeyden <trycollens@gmail.com>
2021-03-09 17:56:26 +0300

    added/logos

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -196,16 +196,39 @@
     <p>Партнеры</p>
     <div class="partners__logo_container">
       <div>
+        <a href="">
         <img src="assets/img/air_logo.svg" alt="">
+        </a>
       </div>
       <div>
+      <a href="http://www.smena.org/">
         <img src="assets/img/smena_logo.svg" alt="">
+      </a>
       </div>
       <div>
+      <a href="https://xn--80afcdbalict6afooklqi5o.xn--p1ai/">
         <img src="assets/img/pgrants_logo_gp_gark.png" alt="">
+      </a>
       </div>
       <div>
+      <a href="https://vk.com/myhistory_omsk">
         <img src="assets/img/pdas.svg" alt="" style="    max-height: 100%;">
+      </a>
+      </div>
+      <div>
+      <a href="https://vk.com/tur_omsk">
+        <img src="assets/img/logo_white.png" alt="">
+      </a>
+      </div>
+      <div>
+      <a href="https://hnkm.ru/">
+        <img src="assets/img/Логотип ХНКМ-01.png" alt="">
+      </a>
+      </div>
+      <div>
+      <a href="https://vk.com/molprav_55">
+        <img src="assets/img/pererisovanny_Montazhnaya_oblast_1.png" alt="">
+      </a>
       </div>
     </div>
   </div>
