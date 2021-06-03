e000ae3205f65ba619c6a91bf07072ea2e3a8a69
laybelmeyden <trycollens@gmail.com>
2021-06-03 20:06:21 +0300

    adde/video

--- resources/views/pages/photos.blade.php
+++ resources/views/pages/photos.blade.php
@@ -3,11 +3,17 @@
 @section('content')
 <link rel="stylesheet" href="/assets/light/lightgallery.css">
 <div class="container">
-    <h1 class="galls__title">ГАЛЕРЕЯ</h1>
+    <h1 class="galls__title">Практическое пособие</h1>
+    <div style="width:100%; display:flex; align-items:center;">
+    <a href="/assets/oblz/Prakticheskoe_posobie_po_ispolzovaniyu_innovatsionnykh_tekhnicheskikh_sredstv_i_interaktivnykh_tekhnologiy_v_proektakh_po_istoricheskim.pdf"><img style="width:100%; max-width: 350px;" src="/assets/oblz/Oblozhka_Sbornik-1.png" alt=""></a>
+    </div>
+    <h1 class="galls__title">Видео</h1>
+    <iframe class="frames__video" width="100%" src="https://www.youtube.com/embed/qeSZWSmxL8A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <h1 class="galls__title">Фото</h1>
     <div id="lightgallery">
         @foreach($result as $item)
         <a href="/assets/img/light/{{$item}}">
-            <img src="/assets/img/light/{{$item}}" class="lazyload" data-sizes="auto"/>
+            <img src="/assets/img/light/{{$item}}" class="lazyload" data-sizes="auto" />
         </a>
         @endforeach
     </div>
